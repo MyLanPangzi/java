02ea3b3333d8032ebc3791dabad4026326e21e3c
MyLanpangzi <1251723871@qq.com>
2019-05-25 11:52:17 +0800

    feat:(ConfigurationProperties) yml 格式多值绑定 yml格式语法编写

--- initializr-hello/src/test/java/com/xiebo/springboot/initializrhello/InitializrHelloApplicationTests.java
+++ initializr-hello/src/test/java/com/xiebo/springboot/initializrhello/InitializrHelloApplicationTests.java
@@ -1,16 +1,21 @@
 package com.xiebo.springboot.initializrhello;
 
+import com.xiebo.springboot.initializrhello.config.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InitializrHelloApplicationTests {
+    @Autowired
+    Person person;
 
     @Test
     public void contextLoads() {
+        System.out.println(person);
     }
 
 }
