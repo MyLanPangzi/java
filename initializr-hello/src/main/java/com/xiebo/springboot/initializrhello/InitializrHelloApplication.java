831d7f1154a84cee86a941ebcbb50b8e763a3ed0
MyLanpangzi <1251723871@qq.com>
2019-05-25 09:34:13 +0800

    chore: spring initializr hello

---
+++ initializr-hello/src/main/java/com/xiebo/springboot/initializrhello/InitializrHelloApplication.java
@@ -0,0 +1,13 @@
+package com.xiebo.springboot.initializrhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrHelloApplication.class, args);
+    }
+
+}
