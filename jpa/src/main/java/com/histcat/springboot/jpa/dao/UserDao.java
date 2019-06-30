fc5d87620a80bc646ae9cb1bd6718a05a6408e45
MyLanpangzi <1251723871@qq.com>
2019-06-30 14:55:11 +0800

    feat: jpa

--- jpa/src/main/java/com/histcat/springboot/jpa/dao/UserDao.java
+++ jpa/src/main/java/com/histcat/springboot/jpa/dao/UserDao.java
@@ -2,9 +2,7 @@ package com.histcat.springboot.jpa.dao;
 
 import com.histcat.springboot.jpa.entity.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserDao extends CrudRepository<User, Integer> {
 
 }
