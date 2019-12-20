345e535755d3fb8ecdd7ceff93fa75af4bf1798e
MyLangPangZi <1251723871@qq.com>
2019-12-20 11:19:16 +0800

    feat(websocket-redis):finish

--- websocke-redis/src/main/java/com/hiscat/websocket/redis/message/HelloMessage.java
+++ websocke-redis/src/main/java/com/hiscat/websocket/redis/message/HelloMessage.java
@@ -1,6 +1,13 @@
 package com.hiscat.websocket.redis.message;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelloMessage {
     private String name;
 }
