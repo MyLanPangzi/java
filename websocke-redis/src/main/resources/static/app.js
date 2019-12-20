49677f1ea18b53d054ae36b3438e0a85f222d40d
MyLangPangZi <1251723871@qq.com>
2019-12-20 14:32:12 +0800

    feat(websocket-redis):test multiple topic subscribe

--- websocke-redis/src/main/resources/static/app.js
+++ websocke-redis/src/main/resources/static/app.js
@@ -21,6 +21,9 @@ function connect() {
         stompClient.subscribe('/topic/greetings', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
+        stompClient.subscribe('/topic/world', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
     });
 }
 
@@ -34,6 +37,7 @@ function disconnect() {
 
 function sendName() {
     stompClient.send("/app/hello/greetings", {}, JSON.stringify({'name': $("#name").val()}));
+    stompClient.send("/app/hello/world", {}, JSON.stringify({'name': $("#name").val()}));
 }
 
 function showGreeting(message) {
