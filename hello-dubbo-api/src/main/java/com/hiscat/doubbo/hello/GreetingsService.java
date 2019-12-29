761a2c5c929632a190ac09f6ca4f2fac424c9998
smailXie <1251723871@qq.com>
2019-12-29 22:49:29 +0800

    feat(hell-dubbo):add hello-dubbo api provider consumer

---
+++ hello-dubbo-api/src/main/java/com/hiscat/doubbo/hello/GreetingsService.java
@@ -0,0 +1,14 @@
+package com.hiscat.doubbo.hello;
+
+/**
+ * @author Administrator
+ */
+public interface GreetingsService {
+    /**
+     * 打招呼
+     *
+     * @param name 姓名
+     * @return 问候
+     */
+    String sayHi(String name);
+}
---
+++ hello-dubbo-provider/src/main/java/com/hiscat/dubbo/hello/ProviderApplication.java
@@ -0,0 +1,28 @@
+package com.hiscat.dubbo.hello;
+
+import com.hiscat.doubbo.hello.GreetingsService;
+import com.hiscat.dubbo.hello.provider.GreetingsServiceImpl;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.ServiceConfig;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Administrator
+ */
+public class ProviderApplication {
+    private static String zookeeperHost = System.getProperty("zookeeper.address", "127.0.0.1");
+
+    public static void main(String[] args) throws Exception {
+        ServiceConfig<GreetingsService> service = new ServiceConfig<>();
+        service.setApplication(new ApplicationConfig("first-dubbo-provider"));
+        service.setRegistry(new RegistryConfig("zookeeper://" + zookeeperHost + ":2181"));
+        service.setInterface(GreetingsService.class);
+        service.setRef(new GreetingsServiceImpl());
+        service.export();
+
+        System.out.println("dubbo service started");
+        new CountDownLatch(1).await();
+    }
+}
