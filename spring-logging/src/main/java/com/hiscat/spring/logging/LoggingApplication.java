ce229aeb633db31625599fa02937bd3406c91b80
MyLangPangZi <1251723871@qq.com>
2020-01-10 16:56:13 +0800

    refactor(spring-logging):refactor logback-spring.xml

--- spring-logging/src/main/java/com/hiscat/spring/logging/LoggingApplication.java
+++ spring-logging/src/main/java/com/hiscat/spring/logging/LoggingApplication.java
@@ -1,5 +1,6 @@
 package com.hiscat.spring.logging;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Administrator
  */
 @SpringBootApplication
+@Slf4j
 public class LoggingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoggingApplication.class, args);
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
+        LOGGER.info("info");
+        LOGGER.warn("warn");
+        LOGGER.error("error");
     }
 
 }
