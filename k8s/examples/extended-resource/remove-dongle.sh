f6b3e8ae5e3e995a3e0e25b590618acbafe0fbe9
MyLanpangzi <1251723871@qq.com>
2020-01-21 20:57:59 +0800

    feat(k8s):add extended example

---
+++ k8s/examples/extended-resource/remove-dongle.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl --header "Content-Type: application/json-patch+json" \
+--request PATCH \
+--data '[{"op": "remove", "path": "/status/capacity/example.com~1dongle"}]' \
+http://localhost:8001/api/v1/nodes/k8s-node1/status
