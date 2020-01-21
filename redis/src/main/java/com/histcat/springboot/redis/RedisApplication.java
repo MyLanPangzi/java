3897927b82df99801a8de5b82d5a72a29615f808
MyLanpangzi <1251723871@qq.com>
2020-01-21 08:02:49 +0800

    feat(redis):add docker-compose.yml

--- redis/src/main/java/com/histcat/springboot/redis/RedisApplication.java
+++ redis/src/main/java/com/histcat/springboot/redis/RedisApplication.java
@@ -1,13 +1,26 @@
 package com.histcat.springboot.redis;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
-public class RedisApplication {
+@AllArgsConstructor
+public class RedisApplication implements CommandLineRunner {
+
+    StringRedisTemplate stringRedisTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        stringRedisTemplate.opsForValue().set("hello", "world");
+    }
 }
