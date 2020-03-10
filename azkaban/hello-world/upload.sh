6065b80f09c4675ad72eaabd66ce78e95c4dedc9
1251723871@qq.com <smailXie@163.com>
2020-03-10 22:27:16 +0800

    feat(azkaban): add java.flow

--- azkaban/hello-world/upload.sh
+++ azkaban/hello-world/upload.sh
@@ -4,4 +4,4 @@ cd  /e/github/java/azkaban/hello-world && 7z a -tzip hello.zip *.flow *.project 
 #上传
 curl -i -X POST --form 'session.id=ae9904e5-6c49-45bc-bd83-7b302d7b0cae' --form 'ajax=upload' --form 'file=@hello.zip;type=application/zip' --form 'project=hello' http://hadoop100:8081/manager
 #运行
-curl -XPOST --data 'session.id=ae9904e5-6c49-45bc-bd83-7b302d7b0cae' --data 'ajax=executeFlow' --data 'project=hello' --data 'flow=basic' http://hadoop100:8081/executor
+curl -XPOST --data 'session.id=ae9904e5-6c49-45bc-bd83-7b302d7b0cae' --data 'ajax=executeFlow' --data 'project=hello' --data 'flow=java' http://hadoop100:8081/executor
