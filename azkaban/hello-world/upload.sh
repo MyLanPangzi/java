d2f68e013d98211273438246979b589a9058ac46
1251723871@qq.com <smailXie@163.com>
2020-03-11 08:27:02 +0800

    refactor(azkaban): refactor hive.flow

--- azkaban/hello-world/upload.sh
+++ azkaban/hello-world/upload.sh
@@ -2,6 +2,10 @@
 #打包
 cd  /e/github/java/azkaban/hello-world && 7z a -tzip hello.zip *.flow *.project || exit
 #上传
-curl -i -X POST --form 'session.id=ae9904e5-6c49-45bc-bd83-7b302d7b0cae' --form 'ajax=upload' --form 'file=@hello.zip;type=application/zip' --form 'project=hello' http://hadoop100:8081/manager
+curl -i -X POST --form 'session.id=5d255b9e-23eb-4de3-9aa1-1db377941b51' --form 'ajax=upload' --form 'file=@hello.zip;type=application/zip' --form 'project=hello' http://hadoop100:8081/manager
 #运行
-curl -XPOST --data 'session.id=ae9904e5-6c49-45bc-bd83-7b302d7b0cae' --data 'ajax=executeFlow' --data 'project=hello' --data 'flow=java' http://hadoop100:8081/executor
+curl -XPOST --data 'session.id=5d255b9e-23eb-4de3-9aa1-1db377941b51' --data 'ajax=executeFlow' --data 'project=hello' --data 'flow=hive' http://hadoop100:8081/executor
+curl  -X POST --data "action=login&username=azkaban&password=azkaban" http://hadoop100:8081/
+curl --get --data "session.id=5d255b9e-23eb-4de3-9aa1-1db377941b51&ajax=fetchFlowExecutions&project=hello&flow=test&start=0&length=3" http://hadoop100:8081/manager
+curl --data "session.id=5d255b9e-23eb-4de3-9aa1-1db377941b51&ajax=fetchexecflow&execid=304" http://localhost:8081/executor
+
